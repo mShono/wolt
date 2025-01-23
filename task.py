67cecf3be9436d6c894c7099375940fdf1d96e79
Mariia Samokhina <mshono@yandex.ru>
2025-01-23 13:48:35 +0200

    0.2 Corrected the coordinate range and added the KeyError

--- task.py
+++ task.py
@@ -31,7 +31,7 @@ def calculate_delivery_fee(base_price, distance_ranges, distance):
         if range["min"] <= distance <= range["max"]:
             delivery_fee = base_price + range["a"] + range["b"] * round(distance / 10)
             return delivery_fee
-    raise HTTPException(status_code=404, detail="The distance is too long, delivery is not possible")
+    raise HTTPException(status_code=200, detail="The distance is too long, delivery is not possible")
 
 
 @app.get(
@@ -41,23 +41,27 @@ def calculate_delivery_fee(base_price, distance_ranges, distance):
 async def get_delivery_order_price(
     venue_slug: str = Query(...),
     cart_value: int = Query(..., ge=0),
-    user_lat: float = Query(...),
-    user_lon: float = Query(...)
+    user_lat: float = Query(..., ge=-90, le=90),
+    user_lon: float = Query(..., ge=-180, le=180)
 ):
     static_data, dynamic_data = await fetch_venue_data(venue_slug)
-    venue_cords_lon, venue_cords_lat = static_data["venue_raw"]["location"]["coordinates"]
-    delivery_specs = dynamic_data["venue_raw"]["delivery_specs"]
+    try:
+        venue_cords_lon, venue_cords_lat = static_data["venue_raw"]["locationss"]["coordinates"]
+        delivery_specs = dynamic_data["venue_raw"]["delivery_specs"]
+    except KeyError:
+        raise HTTPException(
+            status_code=503,
+            detail="We are unable to process your request right now. Sorry for the inconvenience."
+        )
 
     distance = calculate_distance((venue_cords_lat, venue_cords_lon), (user_lat, user_lon))
 
     order_min = delivery_specs["order_minimum_no_surcharge"]
     small_order_surcharge = max(0, order_min - cart_value)
-    print(f"type_small_order_surcharge = {type(small_order_surcharge)}")
 
     base_price = delivery_specs["delivery_pricing"]["base_price"]
     distance_ranges = delivery_specs["delivery_pricing"]["distance_ranges"]
     delivery_fee = calculate_delivery_fee(base_price, distance_ranges, distance)
-    print(f"type_delivery_fee = {type(delivery_fee)}")
 
     return {
         "total_price": int(cart_value + small_order_surcharge + delivery_fee),
